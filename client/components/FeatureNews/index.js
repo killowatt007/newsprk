06bb0fa403ea3fba9cb715c60b29cd8cc04b675b
Denis <denis.samofalov@bk.ru>
2023-04-10 15:10:45 +0300

    home bug style2

--- client/components/FeatureNews/index.js
+++ client/components/FeatureNews/index.js
@@ -1,46 +1,46 @@
 import React, {useState} from 'react';
 import Heading from "../uiStyle/Heading";
-// import Swiper from 'react-id-swiper';
+import Swiper from 'react-id-swiper';
 
-import fnewsImg2 from '../../doc/img/feature/feature2.jpg';
-import fnewsImg3 from '../../doc/img/feature/feature3.jpg';
-import fnewsImg4 from '../../doc/img/feature/feature4.jpg';
+// import fnewsImg2 from '../../doc/img/feature/feature2.jpg';
+// import fnewsImg3 from '../../doc/img/feature/feature3.jpg';
+// import fnewsImg4 from '../../doc/img/feature/feature4.jpg';
 // import {Link} from "react-router-dom";
 import FontAwesome from "../uiStyle/FontAwesome";
 
 const news = [
     {
-        image: fnewsImg2,
+        image: '/img/feature/feature2.jpg',
         category: 'TECHNOLOGY',
         date: 'March 26, 2020',
         title: 'Best garden wing supplies for the horticu ltural'
     },
     {
-        image: fnewsImg3,
+        image: '/img/feature/feature3.jpg',
         category: 'TECHNOLOGY',
         date: 'March 26, 2020',
         title: 'Copa America: Luis Suarez from devastated US'
     },
     {
-        image: fnewsImg4,
+        image: '/img/feature/feature4.jpg',
         category: 'TECHNOLOGY',
         date: 'March 26, 2020',
         title: 'Best garden wing supplies for the horticu ltural'
     },
     {
-        image: fnewsImg3,
+        image: '/img/feature/feature3.jpg',
         category: 'TECHNOLOGY',
         date: 'March 26, 2020',
         title: 'Copa America: Luis Suarez from devastated US'
     },
     {
-        image: fnewsImg4,
+        image: '/img/feature/feature4.jpg',
         category: 'TECHNOLOGY',
         date: 'March 26, 2020',
         title: 'Best garden wing supplies for the horticu ltural'
     },
     {
-        image: fnewsImg3,
+        image: '/img/feature/feature3.jpg',
         category: 'TECHNOLOGY',
         date: 'March 26, 2020',
         title: 'Copa America: Luis Suarez from devastated US'
@@ -96,24 +96,32 @@ const FeatureNews = ({className}) => {
                     <div className="col-12">
                         {/*CAROUSEL START*/}
                         <div className="feature_carousel nav_style1">
-                            {/* <Swiper getSwiper={setSwiper} {...params}> */}
+                            <Swiper getSwiper={setSwiper} {...params}>
                                 {news.map((item, i) => (
-                                    <div></div>
-                                    // <div key={i} className="single_post post_type6 post_type7">
-                                    //     <div className="post_img gradient1">
-                                    //         {/* <Link to="/"><img src={item.image} alt="thumb"/></Link> */}
-                                    //     </div>
-                                    //     <div className="single_post_text">
-                                    //         {/* <div className="meta5"><Link to="/">{item.category}</Link>
-                                    //             <Link to="/">{item.date}</Link>
-                                    //         </div> */}
-                                    //         <h4>
-                                    //             {/* <Link to="/post1">{item.title}</Link> */}
-                                    //         </h4>
-                                    //     </div>
-                                    // </div>
+                                    <div key={i} className="single_post post_type6 post_type7">
+                                        <div className="post_img gradient1">
+                                            {/* <Link to="/"> */}
+                                                <img src={item.image} alt="thumb"/>
+                                            {/* </Link> */}
+                                        </div>
+                                        <div className="single_post_text">
+                                            <div className="meta5">
+                                                {/* <Link to="/"> */}
+                                                    {item.category}
+                                                {/* </Link> */}
+                                                {/* <Link to="/"> */}
+                                                    {item.date}
+                                                {/* </Link> */}
+                                            </div>
+                                            <h4>
+                                                {/* <Link to="/post1"> */}
+                                                    {item.title}
+                                                {/* </Link> */}
+                                            </h4>
+                                        </div>
+                                    </div>
                                 ))}
-                            {/* </Swiper> */}
+                            </Swiper>
                             <div className="navBtns">
                                 <div onClick={goPrev} className="navBtn prevtBtn"><FontAwesome name="angle-left"/></div>
                                 <div onClick={goNext} className="navBtn nextBtn"><FontAwesome name="angle-right"/></div>
